7ac3c52f1c74259d98956c38cd2ef74110d34c51
jplusequalt <jplusequalt@gmail.com>
2022-05-09 16:02:53 -0500

    refactored update to use map instead of a for loop

--- src/app/update.rs
+++ src/app/update.rs
@@ -5,7 +5,7 @@ use crate::utils;
 use crate::App;
 use clap::{CommandFactory, ErrorKind};
 use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 
 impl Run for Update {
     fn run(&self) -> Result<()> {
@@ -20,80 +20,89 @@ impl Run for Update {
             }
         };
 
-        let mut file = OpenOptions::new().read(true).write(true).open(&p).unwrap();
+        let file = OpenOptions::new().read(true).write(true).open(&p).unwrap();
 
-        let mut s = String::new();
-        file.read_to_string(&mut s)?;
+        let mut alias = String::new();
+
+        let mut lines = BufReader::new(&file)
+            .lines()
+            .map(|line| line.unwrap())
+            .filter(|line| {
+                if line.starts_with(format!("alias {}=", self.name).as_str()) {
+                    alias = line.clone();
+                    return false;
+                }
+                true
+            })
+            .collect::<Vec<String>>();
+
         drop(file);
 
-        if let None = s.find(format!("alias {}=", self.name).as_str()) {
-            bail!("no alias named {} was found", self.name);
-        }
+        let mut msg = String::new();
 
-        let mut new_buf = Vec::new();
+        if !alias.is_empty() {
+            match (&self.new_name, &self.new_command) {
+                (Some(new_name), None) => {
+                    lines.push(alias.replace(
+                        format!("alias {}", self.name).as_str(),
+                        format!("alias {}", new_name).as_str(),
+                    ));
 
-        let mut which = false;
+                    msg.push_str(
+                        format!(
+                            "Updated alias '{}' name to '{}'",
+                            self.name,
+                            self.new_name.as_ref().unwrap()
+                        )
+                        .as_str(),
+                    );
+                }
+                (None, Some(new_command)) => {
+                    let offset = self.name.len() + 8;
 
-        for line in s.lines() {
-            if line.starts_with(format!("alias {}", self.name).as_str()) {
-                match (&self.new_name, &self.new_command) {
-                    (Some(new_name), None) => {
-                        which = true;
-                        new_buf.push(line.replace(
-                            format!("alias {}", self.name).as_str(),
-                            format!("alias {}", new_name).as_str(),
-                        ));
-                    }
-                    (None, Some(new_command)) => {
-                        let offset = self.name.len() + 8;
+                    lines.push(alias.replace(
+                        &alias[offset..(alias.len() - 1)],
+                        format!("{}", new_command).as_str(),
+                    ));
 
-                        new_buf.push(line.replace(
-                            &line[offset..(line.len() - 1)],
-                            format!("{}", new_command).as_str(),
-                        ));
-                    }
-                    (None, None) => {
-                        let mut cmd = App::command();
-                        cmd.error(
+                    msg.push_str(
+                        format!(
+                            "Updated alias '{}' command: now '{}'",
+                            self.name,
+                            self.new_command.as_ref().unwrap()
+                        )
+                        .as_str(),
+                    );
+                }
+                (None, None) => {
+                    let mut cmd = App::command();
+                    cmd.error(
                             ErrorKind::ArgumentNotFound,
                             "update requires exactly one argument of either --new_name or --new_command",
                         )
                         .exit();
-                    }
-                    _ => {
-                        let mut cmd = App::command();
-                        cmd.error(
-                            ErrorKind::ArgumentNotFound,
-                            "conflicting arguments for update: \
-                                     use either --new_name or --new_command, but not both",
-                        )
-                        .exit();
-                    }
                 }
-            } else {
-                new_buf.push(line.to_string());
+                _ => {
+                    let mut cmd = App::command();
+                    cmd.error(
+                        ErrorKind::ArgumentNotFound,
+                        "conflicting arguments for update: \
+                                     use either --new_name or --new_command, but not both",
+                    )
+                    .exit();
+                }
             }
+        } else {
+            bail!("no alias named {}", self.name);
         }
 
         let mut new_file = File::create(&p)?;
 
-        for item in new_buf {
+        for item in lines {
             writeln!(new_file, "{}", item)?;
         }
 
-        if which {
-            println!(
-                "Updated alias '{}' name to '{}'",
-                self.name,
-                self.new_name.as_ref().unwrap()
-            );
-        } else {
-            println!(
-                "Updated alias '{}' command: now '{}'",
-                self.name,
-                self.new_command.as_ref().unwrap()
-            );
-        }
+        println!("{}", msg);
 
         Ok(())
     }
